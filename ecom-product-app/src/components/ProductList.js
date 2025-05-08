171fd5dc2bd25aa5e8495904f1e1bb182cc50e97
Joshua D Smith <2smithjosh@gmail.com>
2025-05-08 11:06:19 -0700

    Updated Product Item css

--- ecom-product-app/src/components/ProductList.js
+++ ecom-product-app/src/components/ProductList.js
@@ -5,11 +5,11 @@ const ProductList = ({ products }) => {
     return (
         <div className="product-list">
             {products.map(product => (
-                <ProductItem 
-                    key={product.id} 
-                    name={product.name} 
-                    price={product.price} 
-                    description={product.description} 
+                <ProductItem
+                    key={product.id}
+                    name={product.name}
+                    price={product.price}
+                    description={product.description}
                 />
             ))}
         </div>
